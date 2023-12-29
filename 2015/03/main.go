591ea87d48459d58a62629a0f267547709c68bad
Adrea Snow <adrea.snow@gmail.com>
2023-12-29 19:15:24 +1100

    completed 2015/3

---
+++ 2015/03/main.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+// Let the arrows indicate 2d coordinates
+//     < and > X coordinates and ^ and v are Y
+// Concatenate each coordinate to a string, and use it as the index for a map
+// You can count the entries in the map to see how many houses have been visited
+
+func readData(input string) ([]byte, error) {
+	data, err := os.ReadFile(input)
+	if err != nil {
+		return nil, err
+	}
+	return data, nil
+}
+
+func buildMap(data []byte) map[string]int {
+	coordinates := make(map[string]int)
+
+	x1 := 0
+	y1 := 0
+	x2 := 0
+	y2 := 0
+	u := byte('^')
+	d := byte('v')
+	r := byte('>')
+	l := byte('<')
+
+	var key string
+
+	for count, b := range data {
+		if count%2 == 0 {
+			if b == u {
+				y1++
+			} else if b == d {
+				y1--
+			} else if b == l {
+				x1--
+			} else if b == r {
+				x1++
+			}
+			key = fmt.Sprintf("%05d%05d", x1, y1)
+			coordinates[key] = 1
+		} else {
+			if b == u {
+				y2++
+			} else if b == d {
+				y2--
+			} else if b == l {
+				x2--
+			} else if b == r {
+				x2++
+			}
+			key = fmt.Sprintf("%05d%05d", x2, y2)
+			coordinates[key] = 1
+		}
+	}
+	return coordinates
+}
+
+func main() {
+	data, err := readData("input.txt")
+	if err != nil {
+		fmt.Println(err.Error())
+	} else {
+		coordinates := buildMap(data)
+		fmt.Printf("%d houses have been visited.\n", len(coordinates))
+	}
+}
