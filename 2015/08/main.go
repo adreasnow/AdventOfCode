503f7ddb0849d391245b6adb5e996e61c8bcb360
Adrea Snow <adrea.snow@gmail.com>
2024-01-13 10:13:04 +1100

    completed 2015/08

---
+++ 2015/08/main.go
@@ -0,0 +1,103 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func ReadStrings(fileName string) ([]string, error) {
+	file, err := os.Open(fileName)
+	if err != nil {
+		return nil, err
+	}
+
+	defer file.Close()
+
+	input := make([]string, 0)
+	scanner := bufio.NewScanner(file)
+
+	for scanner.Scan() {
+		input = append(input, scanner.Text())
+	}
+
+	if err := scanner.Err(); err != nil {
+		return nil, err
+	}
+	return input, err
+}
+
+func ScanForHex(s string) string {
+	escapes := strings.Count(s, "\\")
+	for i := 0; i <= escapes; i++ {
+		for count, char := range s {
+			if count+4 <= len(s) {
+				if char == '\\' && s[count+1] == 'x' {
+					_, err := strconv.ParseUint(s[count+2:count+4], 16, 16)
+					if err == nil {
+						replace := string(s[count : count+4])
+						s = strings.Replace(s, replace, "U", 1)
+					}
+				}
+			}
+		}
+	}
+	return s
+}
+
+func Decode(s string) string {
+	s = s[1 : len(s)-1]
+	s = ScanForHex(s)
+
+	s = strings.Replace(s, "\\\\", "|", -1)
+	s = strings.Replace(s, "\\\"", "\"", -1)
+
+	return s
+}
+
+func Encode(s string) string {
+	s = strings.Replace(s, "\\", "\\\\", -1)
+	s = strings.Replace(s, "\"", "\\\"", -1)
+
+	s = fmt.Sprintf("\"%s\"", s)
+	return s
+}
+
+func main() {
+	strings, err := ReadStrings("input.txt")
+	codeChars := 0
+	memChars := 0
+	encodedChars := 0
+
+	var decoded string
+	var encoded string
+	var s string
+
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		for _, s = range strings {
+			codeChars += len(s)
+			decoded = Decode(s)
+			memChars += len(decoded)
+		}
+
+		for _, s = range strings {
+			encoded = Encode(s)
+			encodedChars += len(encoded)
+		}
+
+		for _, s := range []string{"\"\"",
+			"\"abc\"",
+			"\"aaa\\\"aaa\"",
+			"\"\\x27\""} {
+			fmt.Println(Encode(s))
+		}
+
+		fmt.Printf("\nPart A\n----------\nCode: %d\nDecoded: %d\nDifference: %d", codeChars, memChars, codeChars-memChars)
+		fmt.Printf("\n\nPart B\n----------\nCode: %d\nEncoded: %d\nDifference: %d", codeChars, encodedChars, encodedChars-codeChars)
+
+	}
+}
---
+++ 2015/08/main_test.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"testing"
+)
+
+// func TestCheckRepeatWOOverlap(t *testing.T) {
+// 	PassTestCases := []string{"xyxy",
+// 		"aabcdefgaa"}
+// 	for _, s := range PassTestCases {
+// 		if !CheckRepeatWOOverlap(s) {
+// 			t.Errorf("failed %s", s)
+// 		}
+// 	}
+// 	FailTestCases := []string{"aaa"} // only one vowel
+// 	for _, s := range FailTestCases {
+// 		if CheckRepeatWOOverlap(s) {
+// 			t.Errorf("did not fail %s", s)
+// 		}
+// 	}
+// }
+
+func TestDecode(t *testing.T) {
+	strings := map[string]int{"\"\"": 0,
+		"\"abc\"":        3,
+		"\"aaa\\\"aaa\"": 7,
+		"\"\\x27\"":      1}
+
+	var decodedLen int
+	for s, i := range strings {
+		decodedLen = len(Decode(s))
+		if decodedLen != i {
+			t.Errorf("Decoded %s to len %d instead of %d", s, decodedLen, i)
+		}
+	}
+}
+
+func TestEncode(t *testing.T) {
+	strings := map[string]int{"\"\"": 6,
+		"\"abc\"":        9,
+		"\"aaa\\\"aaa\"": 16,
+		"\"\\x27\"":      11}
+
+	var encodedLen int
+	for s, i := range strings {
+		encodedLen = len(Encode(s))
+		if encodedLen != i {
+			t.Errorf("Decoded %s to len %d instead of %d", s, encodedLen, i)
+		}
+	}
+}
